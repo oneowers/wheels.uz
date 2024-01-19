57219842e14965fa09012d9cdf16642063c95d8b
Muhammad Kamolov <muhammadkamolov@MacBook-Air-Muhammad.local>
2024-01-20 01:35:19 +0500

    start

--- netlify/functions/API/main.go
+++ netlify/functions/API/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-  "log"
-  "time"
-
-  "github.com/aws/aws-lambda-go/events"
-  "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
+func generateResponseBody(text string) string {
+	return "Hello, " + text + "!"
+}
+
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-  for i := 0; i < 60; i++ {
-    log.Println(i)
-    time.Sleep(1 * time.Second)
-  }
-  return nil, nil
+	textFromBody := request.Body
+
+	body := generateResponseBody(textFromBody)
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       body,
+	}, nil
 }
 
 func main() {
-  lambda.Start(handler)
+	lambda.Start(handler)
 }
