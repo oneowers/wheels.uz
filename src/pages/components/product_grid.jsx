89abd77d12b28df064e31f0d8b22b1829a4415af
Muhammad Kamolov <muhammadkamolov@MacBook-Air-Muhammad.local>
2024-03-01 05:58:00 +0500

    ByFinch

--- src/pages/components/product_grid.jsx
+++ src/pages/components/product_grid.jsx
@@ -1,68 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import ProductCart from './product_cart.jsx';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import ProductCart from "./product_cart.jsx";
+import { Link } from "react-router-dom";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-  }
+  return classes.filter(Boolean).join(" ");
+}
 
-const ProductItem = ({ productsCount, api , link, name}) => {
-
+const ProductItem = ({ productsCount, api, link, name }) => {
   const [data, setData] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("https://corsproxy.io/?http://linkbuy.uz/api/wheels/", {
+        const response = await fetch(api, {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
           },
         });
-  
+
         const jsonData = await response.json();
         setData(jsonData);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
-  
+
     fetchData();
   }, []);
 
-return (
-  <>
-        <div className="bg-white">
-          <div className="mx-auto max-w-2xl px-4  sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8">
-            {name && (
-            <div className="pt-7 pb-5 sm:pt-7">
-            <div className="flex justify-between space-x-4 items-center">
-                <h2 className={classNames("text-3xl font-medium text-gray-900")}>{name}</h2>
-                {link && (<Link to={link}
-                className={classNames("cursor-pointer whitespace-nowrap text-sm font-medium text-gray-600 hover:text-gray-900")}>
-                View all
-                <span aria-hidden="true"> &rarr;</span>
-                </Link>
-                )}
-            </div>
-            </div>
-            )}
-            <div id="content-container" className="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-              {data ? data.map((item, index) => (index < productsCount) && (
-                <>
-                <ProductCart item={item}/>
-                </>
-              )):
-                [0 ,1, 2, 3, 4, 5, 6, 7].map(() => (
-                  <div className="group h-20 w-full animate-pulse bg-gray-300 rounded-lg">
+  return (
+    <>
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4  sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8">
+          {data ? (
+            data.length > 0 ? (
+              <>
+                <div className="pt-7 pb-5 sm:pt-7">
+                  <div className="flex justify-between space-x-4 items-center">
+                    <h2
+                      className={classNames(
+                        "text-3xl font-medium text-gray-900"
+                      )}
+                    >
+                      Шины для {data[0].category.name}
+                    </h2>
+                    {link && (
+                      <Link
+                        to={"/"}
+                        className={classNames(
+                          "cursor-pointer whitespace-nowrap text-sm font-medium text-gray-600 hover:text-gray-900"
+                        )}
+                      >
+                        View all
+                        <span aria-hidden="true"> &rarr;</span>
+                      </Link>
+                    )}
                   </div>
+                </div>
+                <div
+                  id="content-container"
+                  className="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
+                >
+                  {data &&
+                    data.map(
+                      (item, index) =>
+                        index < productsCount && (
+                          <>
+                            <ProductCart item={item} />
+                          </>
+                        )
+                    )}
+                </div>
+              </>
+            ) : (
+              <div className="flex flex-col items-center justify-center h-20">
+                <p className="text-gray-500 mb-4">
+                  Нет подходящих шин для этого автомобиля.
+                </p>
+                <Link
+                  to="/"
+                  className="text-blue-500 underline hover:text-blue-700"
+                >
+                  Вернуться на главную страницу
+                </Link>
+              </div>
+            )
+          ) : (
+            <div
+              id="content-container"
+              className="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
+            >
+              {[0, 1, 2, 3, 4, 5, 6, 7].map(() => (
+                <div className="group h-20 w-full animate-pulse bg-gray-300 rounded-lg"></div>
               ))}
             </div>
-          </div>
+          )}
         </div>
-      </>
-
-);
+      </div>
+    </>
+  );
 };
 
 export default ProductItem;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import ProductGrid from './components/product_grid.jsx';
 import FAQSection from './components/fAQSection.jsx';
 import Description from './components/description.jsx';
+import { useParams } from 'react-router-dom';
 
 const App = ({ api }) => {
+  const { id: params } = useParams();
+
   return (
     <>
-      <ProductGrid productsCount={4} api={"wheels"}  link="/wheels/" name="Wheels" />
+      <ProductGrid productsCount={4} api={`https://corsproxy.io/?http://linkbuy.uz/api/wheels/?category_id=${params}`} params={useParams()} link="/wheels/" name="Wheels" />
       {/* <Description />
       <FAQSection /> */}
     </>
